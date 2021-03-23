22bb9f80bfdf3d373194e75f2cfc1f8b165bc351
Marcos Mantovani <mvmes23@gmail.com>
2021-03-22 22:18:22 -0300

    adicionado valores aos radio Btns na hora que da o submit

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const login = document.getElementById('button-login');
 
-function clickLogin() {
-  const valor = document.getElementById('user-email-phone');
+// function clickLogin() {
+//   const valor = document.getElementById('user-email-phone');
 
-  alert(valor.value);
-}
+//   alert(valor.value);
+// }
 
-login.addEventListener('click', clickLogin);
+// login.addEventListener('click', clickLogin);
+
+// ou podemos refatorar para o seguinte código para ficar mais simples:
+
+const clickLogin = () => {
+  login.addEventListener('click', () => {
+    const valor = document.getElementById('user-email-phone');
+    window.alert(valor.value);
+  });
+};
+
+window.onload = () => {
+  clickLogin();
+};
